d4fbe88b260cedea8c18e2c793167b2f618f2b43
unknown <samuel.johansson@aalto.fi>
2021-02-10 17:57:54 +0200

    Added interactable object.

---
+++ src/interactableobject.py
@@ -0,0 +1,9 @@
+class InteractableObject():
+	def __init__(self):
+		in_closerange = False
+		
+	def get_in_closerange(self):
+		return in_closerange
+		
+	def set_in_closerange(self, value):
+		in_closerange = value
